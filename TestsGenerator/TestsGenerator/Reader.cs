88c89a46059d43dd23e51fc291af8c459949b885
Pavel <pavelkupr88@gmail.com>
2018-11-25 15:13:48 +0300

    Add reader

---
+++ TestsGenerator/TestsGenerator/Reader.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace TestsGenerator
+{
+    public class Reader
+    {
+		public async Task<string> ReadFromFileAsync(string path)
+		{
+			using (StreamReader reader = new StreamReader(path))
+			{
+				return await reader.ReadToEndAsync();
+			}
+		}
+	}
+}

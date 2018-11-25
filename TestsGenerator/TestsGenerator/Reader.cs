ad6d4ea541cdc2c8886a106dfcf5cb8a59074499
Pavel <pavelkupr88@gmail.com>
2018-11-25 21:01:19 +0300

    Reader class upgrade

--- TestsGenerator/TestsGenerator/Reader.cs
+++ TestsGenerator/TestsGenerator/Reader.cs
@@ -1,16 +1,51 @@
 ﻿using System.IO;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace TestsGenerator
 {
     public class Reader
     {
-		public async Task<string> ReadFromFileAsync(string path)
+		private int threadsCount;
+		public Reader(int threadsCount)
+		{
+			this.threadsCount = threadsCount;
+		}
+
+		public List<Task<string>> ReadFromFilesAsync(List<string> paths)
+		{
+			List<Task<string>> readTasks = new List<Task<string>>();
+			foreach (string path in paths)
+			{
+				readTasks.Add(ReadFromFile(path));
+			}
+
+		}
+
+		private async void RunFreeReadTask(List<Task<string>> readTasks)
+		{
+			foreach(Task<string> readTask in readTasks)
+			{
+				if (readTask.Status == TaskStatus.Created)
+					await readTask;
+			}
+			
+		}
+
+		private Task<string> ReadFromFile(string path)
+		{
+			using (StreamReader reader = new StreamReader(path))
+			{
+				return new Task<string>(reader.ReadToEnd);
+			}
+		}
+		/*
+		private async Task<string> ReadFromFileAsync(string path)
 		{
 			using (StreamReader reader = new StreamReader(path))
 			{
 				return await reader.ReadToEndAsync();
 			}
-		}
+		}*/
 	}
 }
---
+++ TestsGenerator/TestsGeneratorConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGeneratorConsole
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Task<string> readTasks = new Task<string>(ReadFromFileAsync);
+			Task<string>  l = Test(readTasks);
+			l.Wait();
+		}
+		static string ReadFromFileAsync()
+		{
+			System.Threading.Thread.Sleep(2000);
+			System.Threading.Thread.Sleep(2000);
+			return "lol";
+		}
+		static async Task<string> Test(Task<string> readTasks)
+		{
+			readTasks.Start();
+			await readTasks;
+			return "test";
+		}
+	}
+}
